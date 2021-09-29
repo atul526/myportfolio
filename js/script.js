3186de194780c339500373b0b772898f98abb14c
Atul chaudhary <91272081+atul526@users.noreply.github.com>
2021-09-29 20:39:42 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+/*for menu*/
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+/*For music*/
 var mysong = document.getElementById("mysong");
 var icon = document.getElementById("icon")
 icon.onclick = function(){
